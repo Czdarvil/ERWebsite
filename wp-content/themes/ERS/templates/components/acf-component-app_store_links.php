ec2d77cbbd95d8d315063e77ca69d50635919d4c
CurtisL <curtis@solegraphics.com>
2015-03-23 10:37:40 -0700

    App store link component and assets

---
+++ wp-content/themes/ERS/templates/components/acf-component-app_store_links.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * App Stores
+ */
+use Roots\Sage\Assets
+ ?>
+<div class="app-stores">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="row">
+          <?php
+            $stores = get_sub_field('stores');
+            $count = count( $stores );
+            $grid = 12 / $count % 12;
+
+            foreach ($stores as $store) : ?>
+
+              <div class="col-sm-<?php echo $grid ?>">
+                <a href="<?php the_sub_field($store); ?>" class="store-icon <?php echo $store; ?>">
+                  <?php $asset = 'images/app-store/'.$store.'.png'; ?>
+                  <img src="<?php echo Assets\asset_path($asset); ?>" alt="<?php echo $store; ?> app store">
+                </a>
+              </div>
+
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
