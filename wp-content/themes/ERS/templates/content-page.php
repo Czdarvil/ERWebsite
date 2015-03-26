f9d94b4e6d3be05c26d6a35f81ee64f9ae8b6b2f
CurtisL <curtis@solegraphics.com>
2015-03-26 09:19:48 -0700

    Fixed page with no template not being contained to the grid fixes #11

--- wp-content/themes/ERS/templates/content-page.php
+++ wp-content/themes/ERS/templates/content-page.php
@@ -2,6 +2,11 @@
 use Roots\Sage\Config;
 use Roots\Sage\Wrapper;
  ?>
-
-<?php the_content(); ?>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12">
+      <?php the_content(); ?>
+    </div>
+  </div>
+</div>
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
