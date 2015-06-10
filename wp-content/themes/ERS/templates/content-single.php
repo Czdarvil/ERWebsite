15367c757729859aed29777b74627070677467b3
CurtisL <curtis@solegraphics.com>
2015-06-10 10:22:14 -0700

    Added theme option for social widget locations on blog posts

--- wp-content/themes/ERS/templates/content-single.php
+++ wp-content/themes/ERS/templates/content-single.php
@@ -9,10 +9,20 @@
         ?>
         <h1 class="entry-title first-child"><?php the_title(); ?></h1>
         <?php get_template_part('templates/entry-meta'); ?>
+        <?php
+          if (get_field('enable_social_widget_script', 'options') && in_array('header', get_field('single_post_social_widget_locations', 'options') )  ) {
+            the_field('social_widget_script', 'options');
+          }
+        ?>
       </header>
       <div class="entry-content">
         <?php the_content(); ?>
       </div>
+      <?php
+        if (get_field('enable_social_widget_script', 'options') && in_array('footer', get_field('single_post_social_widget_locations', 'options') )  ) {
+          the_field('social_widget_script', 'options');
+        }
+       ?>
       <?php comments_template('/templates/comments.php'); ?>
     </article>
   <?php endwhile; ?>
--- wp-content/themes/ERS/templates/content.php
+++ wp-content/themes/ERS/templates/content.php
@@ -5,6 +5,10 @@
             <h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <?php get_template_part('templates/entry-meta'); ?>
             <?php
+            if (get_field('enable_social_widget_script', 'options') && in_array('header', get_field('posts_archive_social_widget_locations', 'options') ) ) {
+              the_field('social_widget_script', 'options');
+            }
+            the_field('archive_posts_social_footer', 'options');
             if ( has_post_thumbnail() ) {
               echo '<a href="'.get_permalink().'">';
                 the_post_thumbnail( 'post_feature' );
@@ -19,6 +23,9 @@
             if(get_the_tag_list()) {
                 echo get_the_tag_list('<p class="blog-tags">',' ','</p>');
               }
+            if (get_field('enable_social_widget_script', 'options') && in_array('footer', get_field('posts_archive_social_widget_locations', 'options') ) ) {
+              the_field('social_widget_script', 'options');
+            }
           ?>
           <hr>
         </div>
