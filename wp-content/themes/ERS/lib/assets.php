e1abd78549ba9eb1c7abdf318ad4cec75894e4c8
CurtisL <curtis@solegraphics.com>
2016-03-10 15:23:05 -0800

    Updating theme to support Yoast SEO v3 breaking changes to wp filters by adding acf_yoast.js plugin

--- wp-content/themes/ERS/lib/assets.php
+++ wp-content/themes/ERS/lib/assets.php
@@ -127,6 +127,10 @@ function assets() {
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
+function admin_assets(){
+  wp_enqueue_script('acf_yoast', asset_path('scripts/acf_yoast.js'), ['yoast-seo'], false, true);
+}
+add_action( 'admin_enqueue_scripts', __NAMESPACE__.'\\admin_assets', 100);
 // http://wordpress.stackexchange.com/a/12450
 function jquery_local_fallback($src, $handle = null) {
   static $add_jquery_fallback = false;
