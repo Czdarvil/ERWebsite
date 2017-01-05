0191190d2f43e44525565cc2934241883f82ee28
Ben Greene <ben@solegraphics.com>
2017-01-05 15:49:23 -0800

    refactor: header embed
    Changed the header behavior when 'src=embed' is present.
    Base tag added in header file.
    Added styling to push the dropdown menu to maintain its position when header is hidden

--- wp-content/themes/ERS/templates/header.php
+++ wp-content/themes/ERS/templates/header.php
@@ -1,9 +1,20 @@
 <?php use Roots\Sage\Nav; ?>
-
+<?php if( isset( $_GET['src'] ) && 'embed' == $_GET['src'] ):
+/* This is embedded into (presumably) an iframe without the top part of the header. So we need to push down the nav bar AND set all link's target. */ ?>
+  <base target='_parent'>
+  <style>
+    header .menu-container {
+      padding-top: 50px;
+    }
+    @media (min-width: 992px) {
+      header .menu-container {
+        padding-top: 64px;
+      }
+    }
+  </style>
+<?php endif; ?>
 <header class="banner navbar navbar-default navbar-fixed-top" role="banner">
-  <?php if( isset( $_GET['src'] ) && 'embed' == $_GET['src'] ): ?>
-    <base target="_parent" />
-  <?php else: ?>
+  <?php if( ! (isset( $_GET['src'] ) && 'embed' == $_GET['src'] ) ): ?>
     <div class="top-nav container">
       <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>" rel="home">
         <img alt="<?php bloginfo('name'); ?>" src="<?php echo esc_url( get_theme_mod( 'ers_logo' ) ); ?>">
